1f1f895a96210942e4ffdfbd4c781dfba7773b08
akanj-ops <shaheedhaanjum@gmail.com>
2026-06-29 16:34:02 +0530

    added 2 tf files

---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "aws_region" {
+  description = "AWS region to deploy into"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+  # Amazon Linux 2 AMI (update with region-specific ID)
+  default     = "ami-0c55b159cbfafe1f0"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
