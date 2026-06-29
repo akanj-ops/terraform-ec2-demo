f1c3c6ac080e419e092f1ab94086b4074a86db90
akanj-ops <shaheedhaanjum@gmail.com>
2026-06-29 18:50:07 +0530

    updated main.tf

--- main.tf
+++ main.tf
@@ -10,3 +10,8 @@ resource "aws_instance" "demo" {
     Name = "spacelift-demo-ec2"
   }
 }
+
+resource "aws_ec2_instance_state" "demo_server_state" {
+  instance_id = aws_instance.demo.id
+  state       = "running"
+}
